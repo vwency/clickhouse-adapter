62d371a22d8d19fe62983d13648c1779d150bbfd
vwency <veniaminshp@gmail.com>
2025-10-08 22:02:05 +0600

    chore: connection options

--- clickhouse-orm/src/infrastructure/adapters/client/client.rs
+++ clickhouse-orm/src/infrastructure/adapters/client/client.rs
@@ -1,5 +1,5 @@
 use crate::domain::client::client::CHClient;
-use clickhouse::Client;
+use clickhouse::{Client, Compression};
 use std::sync::Arc;
 
 impl CHClient {
@@ -13,6 +13,7 @@ impl CHClient {
         Self { inner: Arc::new(client) }
     }
 
+    /// Создает клиент с полными учетными данными
     pub fn with_credentials(
         url: impl Into<String>,
         database: impl Into<String>,
@@ -27,7 +28,73 @@ impl CHClient {
         Self { inner: Arc::new(client) }
     }
 
+    pub fn builder() -> CHClientBuilder {
+        CHClientBuilder::new()
+    }
+
     pub(crate) fn client(&self) -> &Client {
         &self.inner
+    }
+}
+
+pub struct CHClientBuilder {
+    client: Client,
+}
+
+impl CHClientBuilder {
+    pub fn new() -> Self {
+        Self { client: Client::default() }
+    }
+
+    pub fn url(mut self, url: impl Into<String>) -> Self {
+        self.client = self.client.with_url(url.into());
+        self
+    }
+
+    pub fn database(mut self, database: impl Into<String>) -> Self {
+        self.client = self.client.with_database(database.into());
+        self
+    }
+
+    pub fn user(mut self, user: impl Into<String>) -> Self {
+        self.client = self.client.with_user(user.into());
+        self
+    }
+    pub fn password(mut self, password: impl Into<String>) -> Self {
+        self.client = self.client.with_password(password.into());
+        self
+    }
+
+    pub fn header(mut self, key: impl Into<String>, value: impl Into<String>) -> Self {
+        self.client = self.client.with_header(key.into(), value.into());
+        self
+    }
+
+    pub fn option(mut self, name: impl Into<String>, value: impl Into<String>) -> Self {
+        self.client = self.client.with_option(name.into(), value.into());
+        self
+    }
+
+    pub fn compression(mut self, compression: Compression) -> Self {
+        self.client = self.client.with_compression(compression);
+        self
+    }
+
+    pub fn with_lz4_compression(self) -> Self {
+        self.compression(Compression::Lz4)
+    }
+
+    pub fn with_no_compression(self) -> Self {
+        self.compression(Compression::None)
+    }
+
+    pub fn build(self) -> CHClient {
+        CHClient { inner: Arc::new(self.client) }
+    }
+}
+
+impl Default for CHClientBuilder {
+    fn default() -> Self {
+        Self::new()
     }
 }
